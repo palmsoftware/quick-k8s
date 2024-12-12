85518dcfb3ebad9d4677ac009248269d0b43f134
Brandon Palm <bpalm@redhat.com>
2024-12-12 12:32:15 -0600

    Bootstrap the docker instance

---
+++ scripts/bootstrap-docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# create the docker daemon json file if it does not exist
+sudo touch -a /etc/docker/daemon.json
+
+# update docker config to include ipv6 support
+jq '. +={"ipv6": true, "fixed-cidr-v6": "2001:db8:1::/64"}' /etc/docker/daemon.json > /tmp/new-docker-daemon.json
+sudo cp /tmp/new-docker-daemon.json /etc/docker/daemon.json
+rm /tmp/new-docker-daemon.json
+
+# restart docker
+sudo systemctl restart docker
+
+# increase file system limits
+sudo sysctl fs.inotify.max_user_watches=524288
+sudo sysctl fs.inotify.max_user_instances=512
